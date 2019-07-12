1d54b3b6cd8c1236296b5397123e136c03c10bf4
unknown <labeebabrar222@gmail.com>
2019-07-12 09:59:26 -0400

    practice using return function

--- returnPractice.js
+++ returnPractice.js
@@ -5,3 +5,13 @@ function someFunction()
 
 const result = someFunction();
 console.log(result);
+
+
+console.log("\n");
+
+function addTwoNums(num1, num2)
+{
+    return num1 + num2;
+}
+const sum = addTwoNums(999, 493);
+console.log(sum);
